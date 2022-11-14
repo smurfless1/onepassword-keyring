bfd0f06c4b131ad7cbbcdd30323c913eb27f0ee4
David Brown <dbrown@roku.com>
2022-11-14 16:31:58 -0600

    track better duplicate-title handling

--- onepassword_keyring/__init__.py
+++ onepassword_keyring/__init__.py
@@ -42,28 +42,48 @@ class OnePasswordKeyring(KeyringBackend):
         """
         self._check_onepassword_connection()
 
+    def _get_matching_uuid(self, service: str, username: str, vault: Optional[str] = None) -> Optional[str]:
+        """
+        Get the item fields matching this most closely.
+
+        This will be slower than possible, but will disambiguate logins for you.
+        For instance, my 1password has Airbnb twice with two different usernames.
+        The CLI will fail, but this handles it pre-emptively.
+        :return:
+        """
+        return self.op.get_first_uuid_with_hint(title=service, hint=username, vault=vault)
+
     def get_password(self, service: str, username: str) -> Optional[str]:
+        '''
+        Get the login matching this title and username, and return its password.
+        '''
         self._check_onepassword_connection()
-        response_dict = self.op.get_item_fields(uuid=service, fields=['username', 'password'])
+        uuid = self._get_matching_uuid(service, username)
+        if uuid is None:
+            return 'not found'
+
+        response_dict = self.op.get_item_fields(uuid=uuid, fields=['username', 'password'])
         if response_dict.get('username') == username:
             return response_dict.get('password')
         return 'not found'
 
     def set_password(self, service: str, username: str, password: str) -> None:
         self._check_onepassword_connection()
-        found = self.op.get_item_fields(uuid=service, fields='username')
-        if 'username' in found:
-            self.op.edit_item_username(uuid=service, value=username)
-            self.op.edit_item_password(uuid=service, value=password)
+        uuid = self._get_matching_uuid(service, username)
+        if uuid is None:
+            self.op.create_login(
+                username, password, service
+            )
             return
-        self.op.create_login(
-            username, password, service
-        )
+
+        self.op.edit_item_username(uuid=uuid, value=username)
+        self.op.edit_item_password(uuid=uuid, value=password)
 
     def delete_password(self, service: str, username: str) -> None:
         self._check_onepassword_connection()
-        response_dict = self.op.get_item_fields(uuid=service, fields=['username', 'password'])
-        if response_dict.get('username') == username:
-            self.op.delete_item(uuid=service)
-            return
-        print("Could not match that service and username, not deleting.")
+        uuid = self._get_matching_uuid(service, username)
+        if uuid is None:
+            print("Could not match that service and username, not deleting.")
+            return None
+
+        self.op.delete_item(uuid=uuid)
