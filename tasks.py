bb0fce7394e81aaf5d1d3a791f612e05f39aa981
David Brown <business@smurfless.com>
2022-12-27 12:13:22 -0600

    tasks update

--- tasks.py
+++ tasks.py
@@ -1,6 +1,7 @@
 from invoke import task
 import keyring
 
+
 @task
 def setup_poetry(c):
     api_key = keyring.get_password('gitea API key', 'smurfless1')
@@ -8,11 +9,13 @@ def setup_poetry(c):
     c.run("poetry config repositories.gitea 'http://sinus.smurfless.com:3000/api/packages/smurfless1/pypi'")
     c.run(f"poetry config pypi-token.gitea {api_key}")
 
-@task
-def publish(c):
-    c.run(f"poetry publish --build --repository gitea")
 
 @task
 def build(c):
     c.run("poetry2setup > setup.py")
     c.run("poetry build")
+
+
+@task(build)
+def publish(c):
+    c.run(f"poetry publish --repository gitea")
