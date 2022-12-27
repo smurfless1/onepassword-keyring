5f27c356b65625d1eaf960037567b610d33fa1e8
David Brown <business@smurfless.com>
2022-12-27 12:51:22 -0600

    patch bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ entry_points = \
 
 setup_kwargs = {
     'name': 'onepassword-keyring',
-    'version': '2.0.7',
+    'version': '2.0.9',
     'description': 'keyring wrapper for onepassword module',
     'long_description': 'None',
     'author': 'David Brown',
